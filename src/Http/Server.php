5420909c2524de22c71961b709d4d510c9a64722
Aleksey Shapovalov <gigamel@yandex.ru>
2024-10-26 22:02:41 +0300

    [Http] added Server

---
+++ src/Http/Server.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Gigamel\Http;
+
+use Gigamel\Http\Protocol\ServerMessageInterface;
+
+class Server implements ServerInterface
+{
+    public function sendMessage(ServerMessageInterface $message): void
+    {
+        if (headers_sent()) {
+            return;
+        }
+
+        foreach ($message->getHeaders() as $header => $value) {
+            header(sprintf('%s: %s', $header, $value), true);
+        }
+
+        header(sprintf('%s %d %s', 'HTTP/1.1', $message->getStatusCode(), 'Some Status Message'));
+
+        echo $message->getBody();
+    }
+}
