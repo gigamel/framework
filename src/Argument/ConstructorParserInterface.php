01352e0714ca424cfe79e5d19081754cbd1f24f6
Aleksey Shapovalov <gigamel@yandex.ru>
2024-11-15 20:43:54 +0300

    [Argument] fixed parser class name

--- src/Argument/ConstructorParserInterface.php
+++ src/Argument/ConstructorParserInterface.php
@@ -4,7 +4,7 @@ namespace Gigamel\Argument;
 
 use InvalidArgumentException;
 
-interface ConstructParserInterface
+interface ConstructorParserInterface
 {
     /**
      * @throws InvalidArgumentException
